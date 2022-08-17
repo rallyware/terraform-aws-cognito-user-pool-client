c06fe1d596c8dba8822498f87215a0a31c6b06a7
Vladimir <26582191+SweetOps@users.noreply.github.com>
2022-08-17 16:55:21 +0200

    feat: initial implementation (#1)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,6 +1,68 @@
-module "aweasome_module" {
-  source    = "../../"
+module "label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
   name      = "aweasome"
   stage     = "production"
-  namespace = "sweetops"
+  namespace = "rlw"
+
+}
+
+module "cognito" {
+  source  = "rallyware/cognito-user-pool/aws"
+  version = "0.1.0"
+
+  identity_providers = [
+    {
+      name            = "Google"
+      type            = "Google"
+      idp_identifiers = []
+
+      provider_details = {
+        authorize_scopes              = "openid profile email"
+        client_id                     = "GOOGLE_CLIENT_ID"
+        client_secret                 = "GOOGLE_CLIENT_SECRET"
+        attributes_url                = "https://people.googleapis.com/v1/people/me?personFields="
+        attributes_url_add_attributes = "true"
+        authorize_url                 = "https://accounts.google.com/o/oauth2/v2/auth"
+        oidc_issuer                   = "https://accounts.google.com"
+        token_request_method          = "POST"
+        token_url                     = "https://www.googleapis.com/oauth2/v4/token"
+      }
+
+      attribute_mapping = {
+        email       = "email"
+        family_name = "family_name"
+        given_name  = "given_name"
+        name        = "name"
+        picture     = "picture"
+        username    = "sub"
+      }
+    }
+  ]
+
+  groups = [
+    {
+      name       = "admin"
+      precedence = 1
+    },
+    {
+      name       = "dev"
+      precedence = 10
+    }
+  ]
+
+  context = module.label.context
+
+}
+
+module "cognito_client" {
+  source = "../../"
+
+  user_pool_id                 = module.cognito.id
+  supported_identity_providers = ["Google"]
+  callback_urls                = ["https://grafana.local/login/generic_oauth"]
+
+  context    = module.label.context
+  attributes = ["grafana"]
 }
--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
 locals {
   enabled = module.this.enabled
 }
+
+resource "aws_cognito_user_pool_client" "default" {
+  count = local.enabled ? 1 : 0
+
+  name                                 = module.this.id
+  user_pool_id                         = var.user_pool_id
+  generate_secret                      = var.generate_secret
+  callback_urls                        = var.callback_urls
+  allowed_oauth_flows_user_pool_client = var.allowed_oauth_flows_user_pool_client
+  allowed_oauth_flows                  = var.allowed_oauth_flows
+  allowed_oauth_scopes                 = var.allowed_oauth_scopes
+  explicit_auth_flows                  = var.explicit_auth_flows
+  logout_urls                          = var.logout_urls
+  supported_identity_providers         = var.supported_identity_providers
+  prevent_user_existence_errors        = var.prevent_user_existence_errors
+}
